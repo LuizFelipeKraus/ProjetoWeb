72312da5471606c2186837087de63a457cd2e43b
Luiz <luiz.k1999@aluno.ifsc.edu.br>
2021-06-08 23:42:51 -0300

    Cadastro funcionando

--- database/migrations/2021_06_06_225036_create_table_venda.php
+++ database/migrations/2021_06_06_225036_create_table_venda.php
@@ -16,7 +16,7 @@ class CreateTableVenda extends Migration
         Schema::create('venda', function (Blueprint $table) {
             $table->id();
             $table->decimal('valor_total',15,2);
-            $table->foreignId('id_cliente')->constrained('cliente');
+            $table->foreignId('id_users')->constrained('users');
             $table->foreignId('id_endereco')->constrained('endereco');
             $table->timestamps();
         });
@@ -30,8 +30,8 @@ class CreateTableVenda extends Migration
     public function down()
     {
         Schema::table('venda', function (Blueprint $table) {
-            $table->dropForeign(['id_cliente']);
-            $table->dropColumn('id_cliente');
+            $table->dropForeign(['id_users']);
+            $table->dropColumn('id_users');
             $table->dropForeign(['id_endereco']);
             $table->dropColumn('id_endereco');
         });
--- database/migrations/2021_06_09_022531_alter_table_users.php
+++ database/migrations/2021_06_09_022531_alter_table_users.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateTableCliente extends Migration
+class AlterTableUsers extends Migration
 {
     /**
      * Run the migrations.
@@ -13,14 +13,11 @@ class CreateTableCliente extends Migration
      */
     public function up()
     {
-        Schema::create('cliente', function (Blueprint $table) {
-            $table->id();
-            $table->string('nome');
+        Schema::table('users', function (Blueprint $table) {
             $table->string('cpf');
             $table->string('rg');
             $table->date('data_nascimento');
             $table->string('telefone');
-            $table->timestamps();
         });
     }
 
@@ -31,6 +28,12 @@ class CreateTableCliente extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('cliente');
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('cpf');
+            $table->dropColumn('rg');
+            $table->dropColumn('data_nascimento');
+            $table->dropColumn('telefone');
+        });
+        
     }
 }
