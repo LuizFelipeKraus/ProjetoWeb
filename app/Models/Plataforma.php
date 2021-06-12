713da9e87fb4b20276d0d570715bfc795c3c608d
luiz higashi <luizhigashi2012@gmail.com>
2021-06-12 15:08:57 -0300

    Fim das exclusões

--- app/Models/Plataforma.php
+++ app/Models/Plataforma.php
@@ -5,9 +5,11 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use App\Models\Produto;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
 class Plataforma extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
     protected $table = "plataforma";
 
     function produto(){       
